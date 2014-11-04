8939f8b19446a66346ca309e74992d6e68f60bcb
Eduardo Basílio <eduardoafonsobasilio@gmail.com>
2014-11-04 04:27:19 -0300

    created valid_br_cnpj

--- tests/test_utilplus.py
+++ tests/test_utilplus.py
@@ -26,11 +26,41 @@ def test_valid_br_cpf():
         text = str(i) * 11
         yield check_br_cpf_False, text
 
+    yield check_br_cpf_False, '46736825566'  # invalid cpf
     yield check_br_cpf_False, 46736825563  # no string
-    yield check_br_cpf_False, '467.368.255/63'  # special character =! . or -
+    yield check_br_cpf_False, '467.368.255/63'  # special character =! . -
     yield check_br_cpf_False, '467368255638'  # > 11 digits
     yield check_br_cpf_False, '4673682556'  # < 11 digits
 
 
 def check_br_cpf_False(cpf):
     assert_equal(False, utilplus.valid_br_cpf(cpf))
+
+
+def test_valid_br_cnpj():
+    """
+    Test valid_br_cnpj
+    """
+    # True
+    assert_equal(True, utilplus.valid_br_cnpj('1144477700016'))
+    assert_equal(True, utilplus.valid_br_cnpj('11.444.777/0001-61'))
+
+    # False
+    for i in range(10):
+        text = str(i) * 14
+        yield check_br_cnpj_False, text
+
+    # invalid cnpj
+    assert_equal(False, utilplus.valid_br_cnpj('84917968000166'))
+    # if accept not string
+    assert_equal(False, utilplus.valid_br_cnpj(64746812000163))
+    # if special character =! . / -
+    assert_equal(False, utilplus.valid_br_cnpj('64.746.812/0001,63'))
+    # if lenth > 14
+    assert_equal(False, utilplus.valid_br_cnpj('647468120001631'))
+    # if lenth < 14
+    assert_equal(False, utilplus.valid_br_cnpj('6474681200016'))
+
+
+def check_br_cnpj_False(cnpj):
+    assert_equal(False, utilplus.valid_br_cnpj(cnpj))
--- utilplus.py
+++ utilplus.py
@@ -79,3 +79,74 @@ def valid_br_cpf(cpf):
     assemble_cpf += str(checksum2)
 
     return True if cpf == assemble_cpf else False
+
+
+def valid_br_cnpj(cnpj):
+    """
+    Accept an string parameter cpf and
+    Check if is brazilian CNPJ valid.
+    Return True or False
+    """
+     # if cnpj is string
+    if not isinstance(cnpj, basestring):
+        return False
+
+    cnpj = cnpj.replace('.', '')  # extract dots
+    cnpj = cnpj.replace('-', '')  # extract stroke
+    cnpj = cnpj.replace('/', '')  # extract slash
+
+     # if does not contain numerical characters
+    for i in cnpj:
+        if not i.isdigit():
+            return False
+            break
+
+    #  check if length == 14
+    if len(cnpj) != 14:
+        return False
+
+    # checks if all digits are equal
+    for i in range(10):
+        text = str(i) * 14
+        if text == cnpj:
+            return False
+            break
+
+    # first checksum1
+    multi = 5
+    result = 0
+    for i in cnpj[:12]:
+        result = result + (int(i) * multi)
+        multi -= 1
+        if multi < 2:
+            multi = 9
+
+    remainder = result % 11
+
+    if remainder < 2:
+        checksum1 = 0
+    else:
+        checksum1 = 11 - remainder
+
+    assemble_cnpj = cnpj[:12] + str(checksum1)
+
+    # secound checksum
+    multi = 6
+    result = 0
+
+    for i in assemble_cnpj:
+        result = result + (int(i) * multi)
+        multi = multi - 1
+        if multi < 2:
+            multi = 9
+
+    remainder = result % 11
+
+    if remainder < 2:
+        checksum2 = 0
+    else:
+        checksum2 = 11 - remainder
+
+    assemble_cnpj += str(checksum2)
+
+    return True if cnpj == assemble_cnpj else False
